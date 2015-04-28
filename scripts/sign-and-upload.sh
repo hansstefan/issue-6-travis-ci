44fe3abbe4e9a58fee1600c167077face7711529
Marcin Suszczewicz <m.suszczewicz@gmail.com>
2015-04-28 12:56:07 +0200

    secret test

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -27,31 +27,31 @@ zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" "$OUTPUTDIR/$APP_NAME.app.dSYM"
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 
-if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
-  echo ""
-  echo "***************************"
-  echo "* Uploading to Testflight *"
-  echo "***************************"
-  curl http://testflightapp.com/api/builds.json \
-    -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
-    -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
-    -F api_token="$TESTFLIGHT_API_TOKEN" \
-    -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
-    -F distribution_lists='Internal' \
-    -F notes="$RELEASE_NOTES"
-fi
-
-if [ ! -z "$HOCKEY_APP_ID" ] && [ ! -z "$HOCKEY_APP_TOKEN" ]; then
-  echo ""
-  echo "***************************"
-  echo "* Uploading to Hockeyapp  *"
-  echo "***************************"
-  curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
-    -F status="2" \
-    -F notify="0" \
-    -F notes="$RELEASE_NOTES" \
-    -F notes_type="0" \
-    -F ipa="@$OUTPUTDIR/$APP_NAME.ipa" \
-    -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
-    -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
-fi
+# if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
+#   echo ""
+#   echo "***************************"
+#   echo "* Uploading to Testflight *"
+#   echo "***************************"
+#   curl http://testflightapp.com/api/builds.json \
+#     -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
+#     -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+#     -F api_token="$TESTFLIGHT_API_TOKEN" \
+#     -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
+#     -F distribution_lists='Internal' \
+#     -F notes="$RELEASE_NOTES"
+# fi
+# 
+# if [ ! -z "$HOCKEY_APP_ID" ] && [ ! -z "$HOCKEY_APP_TOKEN" ]; then
+#   echo ""
+#   echo "***************************"
+#   echo "* Uploading to Hockeyapp  *"
+#   echo "***************************"
+#   curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
+#     -F status="2" \
+#     -F notify="0" \
+#     -F notes="$RELEASE_NOTES" \
+#     -F notes_type="0" \
+#     -F ipa="@$OUTPUTDIR/$APP_NAME.ipa" \
+#     -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+#     -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
+# fi
